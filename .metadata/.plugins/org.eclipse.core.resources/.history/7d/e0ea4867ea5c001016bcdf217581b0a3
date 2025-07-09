99da31d26c69225f718aeb6ce3566104b067c575
Wpompeo <wag_pompeo@yahoo.com.br>
2025-07-09 16:28:07 -0300

    new project InventoryControl

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7d/e0ea4867ea5c001016bcdf217581b0a3
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package entities;
+
+/**
+ * @author wag_p
+ *
+ */
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+
+}
--- curso_programacao/src/application/Geometrics.java
+++ curso_programacao/src/application/Geometrics.java
@@ -29,11 +29,11 @@ public class Geometrics {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		x.calculateArea();
+		y.calculateArea();
 
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.calculateArea();
+		double areaY = y.calculateArea();
 
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle y area: %.4f%n", areaY);
