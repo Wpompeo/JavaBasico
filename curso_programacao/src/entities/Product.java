c552eadc62cbb3c2b301901cd53b411fd53b40f5
Wpompeo <wag_pompeo@yahoo.com.br>
2025-07-17 15:07:32 -0300

    New project Bank and TransactionBank

--- curso_programacao/src/entities/Product.java
+++ curso_programacao/src/entities/Product.java
@@ -1,11 +1,31 @@
 package entities;
 
 public class Product {
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 
-	//constructor
+	// construtores
+
+	public Product() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
@@ -13,9 +33,15 @@ public class Product {
 
 	}
 
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+
+	}
+
 	public double totalValueInStock() {
-		Double valueEstoque = (price * quantity);
-		return valueEstoque;
+		return price * quantity;
+
 	}
 
 	public void addProductsInStock(int quantity) {
